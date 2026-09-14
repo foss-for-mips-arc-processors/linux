5a01a82877bda0fa69e3bfc7e39255bd068ae902
Luis Silva <luis.silva@globalfoundries.com>
2026-09-14 09:26:01 +0100

    arc: Rebrand copyright to MIPS
    Signed-off-by: Luis Silva <luis.silva@globalfoundries.com>

--- arch/arc/include/uapi/asm/unistd.h
+++ arch/arc/include/uapi/asm/unistd.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 /*
- * Copyright (C) 2004, 2007-2010, 2011-2012 Synopsys, Inc. (www.synopsys.com)
+ * Copyright (c) 2004 - 2026 MIPS Holding, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
